7bf01487485bded5b13d75d078e61b7d2d9d6ac5
Ben Leedom <africanswallow@gmail.com>
2019-02-26 21:42:19 -0600

    Add empty feature toggles class.

---
+++ Rebar/RebarFeatureToggles.cs
@@ -0,0 +1,11 @@
+﻿using NationalInstruments.FeatureToggles;
+
+namespace Rebar
+{
+    [ExportFeatureToggles]
+    public sealed class RebarFeatureToggles : FeatureTogglesProvider<RebarFeatureToggles>
+    {
+        private const string RebarFeatureCategory = "Rebar";
+        private const string FeaturePrefix = "Rebar.FeatureToggles.";
+    }
+}
