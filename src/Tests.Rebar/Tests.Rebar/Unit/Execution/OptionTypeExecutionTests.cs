f1d7bb0bf29e600014df332b0a549e2cd0686191
benleedom <africanswallow@gmail.com>
2019-05-11 00:47:45 -0500

    [OptionType] Add empty OptionTypeExecutionTests class.

---
+++ src/Tests.Rebar/Tests.Rebar/Unit/Execution/OptionTypeExecutionTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Rebar.Unit.Execution
+{
+    [TestClass]
+    public class OptionTypeExecutionTests : ExecutionTestBase
+    {
+    }
+}
