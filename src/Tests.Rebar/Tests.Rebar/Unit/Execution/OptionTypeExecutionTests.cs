2696f4cc24bf2d0264b2ebd92dbc8cd98f36f9f6
benleedom <africanswallow@gmail.com>
2019-05-11 00:55:24 -0500

    Add execution test for IsSome connected to Some.

--- src/Tests.Rebar/Tests.Rebar/Unit/Execution/OptionTypeExecutionTests.cs
+++ src/Tests.Rebar/Tests.Rebar/Unit/Execution/OptionTypeExecutionTests.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NationalInstruments.DataTypes;
+using NationalInstruments.Dfir;
+using Rebar.Common;
+using Rebar.Compiler.Nodes;
+using Rebar.RebarTarget.Execution;
 
 namespace Tests.Rebar.Unit.Execution
 {
     [TestClass]
     public class OptionTypeExecutionTests : ExecutionTestBase
     {
+        [TestMethod]
+        public void IsSomeWithSomeValueInput_Execute_IsSomeOutputsTrue()
+        {
+            DfirRoot function = DfirRoot.Create();
+            FunctionalNode inspect = new FunctionalNode(function.BlockDiagram, Signatures.InspectType);
+            FunctionalNode isSome = new FunctionalNode(function.BlockDiagram, Signatures.IsSomeType);
+            Wire.Create(function.BlockDiagram, isSome.OutputTerminals[1], inspect.InputTerminals[0]);
+            FunctionalNode some = new FunctionalNode(function.BlockDiagram, Signatures.SomeConstructorType);
+            Wire.Create(function.BlockDiagram, some.OutputTerminals[0], isSome.InputTerminals[0]);
+            ConnectConstantToInputTerminal(some.InputTerminals[0], PFTypes.Int32, false);
+
+            ExecutionContext context = CompileAndExecuteFunction(function);
+
+            byte[] lastValue = GetLastValueFromInspectNode(context, inspect);
+            AssertByteArrayIsInt32(lastValue, 1);
+        }
     }
 }
