34dd9afcf1691cf095eae6351a35b759f80fe615
Ben Leedom <africanswallow@gmail.com>
2019-07-25 23:18:29 -0500

    Register a clipboard format for FunctionDiagramEditor so that copy/paste works.

---
+++ src/Rebar/Design/FunctionDiagramEditorInfo.cs
@@ -0,0 +1,23 @@
+﻿using NationalInstruments.Core;
+using NationalInstruments.Design;
+using NationalInstruments.Shell;
+
+namespace Rebar.Design
+{
+    /// <summary>
+    /// <see cref="DocumentEditControlInfo"/> implementation for <see cref="FunctionDiagramEditor"/>.
+    /// </summary>
+    public class FunctionDiagramEditorInfo : DocumentEditControlInfo<FunctionDiagramEditor>
+    {
+        /// <summary>
+        /// The clipboard format accepted by this sketch diagram
+        /// </summary>
+        private static readonly string FunctionClipboardDataFormat = ClipboardFormatHelper.RegisterClipboardFormat(DragDrop.NIDataFormatPrefix + FunctionDiagramPaletteLoader.DiagramPaletteIdentifier, "RebarFunctionDiagram");
+
+        public FunctionDiagramEditorInfo(string uniqueId, FunctionDocument document)
+            : base(uniqueId, document, document.Function.Diagram, "editor", FunctionDiagramPaletteLoader.DiagramPaletteIdentifier, string.Empty, string.Empty)
+        {
+            ClipboardDataFormat = FunctionClipboardDataFormat;
+        }
+    }
+}
