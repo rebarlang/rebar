b47f8de723939af4655e148da288a8e752f396d4
Ben Leedom <africanswallow@gmail.com>
2020-05-15 17:11:52 -0500

    Create VariantMatchStructureSelectorTerminal and hide it when it is an output with an associated Unit field type.

--- src/Rebar/SourceModel/VariantMatchStructureSelector.cs
+++ src/Rebar/SourceModel/VariantMatchStructureSelector.cs
@@ -1,6 +1,11 @@
-﻿using System.Xml.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using NationalInstruments;
+using NationalInstruments.DataTypes;
 using NationalInstruments.SourceModel;
 using NationalInstruments.SourceModel.Persistence;
+using Rebar.Common;
 
 namespace Rebar.SourceModel
 {
@@ -18,5 +23,63 @@ namespace Rebar.SourceModel
 
         /// <inheritdoc />
         public override XName XmlElementName => XName.Get(ElementName, Function.ParsableNamespaceName);
+
+        private VariantMatchStructure VariantMatchStructure => (VariantMatchStructure)Owner;
+
+        /// <inheritdoc />
+        public override IEnumerable<Terminal> VisibleTerminals
+        {
+            get
+            {
+                yield return PrimaryOuterTerminal;
+                yield return GetTerminalInDiagram(VariantMatchStructure.SelectedDiagram);
+            }
+        }
+
+        /// <inheritdoc />
+        public override Terminal CreateTerminal(string identifier)
+        {
+            return new VariantMatchStructureSelectorTerminal(identifier);
+        }
+    }
+
+    internal class VariantMatchStructureSelectorTerminal : BorderNodeTerminal
+    {
+        public VariantMatchStructureSelectorTerminal()
+        {
+        }
+
+        public VariantMatchStructureSelectorTerminal(string terminalIdentifier)
+        {
+            TerminalIdentifier = terminalIdentifier;
+        }
+
+        private VariantMatchStructureSelector VariantMatchStructureSelector => (VariantMatchStructureSelector)Owner;
+
+        /// <inheritdoc />
+        public override bool Visible
+        {
+            get
+            {
+                if (Direction == NationalInstruments.CommonModel.Direction.Input)
+                {
+                    return true;
+                }
+
+                var inputTerminal = VariantMatchStructureSelector.InputTerminals.First();
+                NIType inputType = inputTerminal.DataType;
+                if (inputType.IsUnion())
+                {
+                    int index = VariantMatchStructureSelector.OutputTerminals.IndexOf(this);
+                    NIType fieldType = inputType.GetFields().ElementAt(index).GetDataType();
+                    if (fieldType.IsUnit())
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
     }
 }
